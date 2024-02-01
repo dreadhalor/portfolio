92e2ffef30dc8b112d48dba9a81752b24dbd47b3
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-01 15:08:26 -0800

    A lot of things are working in the test-ui library, but some filenames are being dumb w/capitalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button, Button2 } from "test-ui";
+
 function App() {
   return (
     <div className="flex h-full w-full items-center justify-center border-8 border-black">
       HI
+      <Button label="hey" />
+      <Button2>hey</Button2>
+      <button className="bg-red-500">huhsdf</button>
     </div>
   );
 }
