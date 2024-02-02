ffd7c0ade3a343e61c3dd4da1471377157955751
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-02 12:05:03 -0800

    Apps in the switcher have icons now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import { Button } from "dread-ui";
 import { useEffect, useState } from "react";
+import {
+  AsciiVideoIcon,
+  EnlightIcon,
+  MinesweeperIcon,
+  PathfinderVisualizerIcon,
+} from "@repo/assets";
+
+const AppIcon = ({
+  src,
+  alt,
+  onClick,
+}: {
+  src?: string;
+  alt: string;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      className="flex h-12 w-12 items-center justify-center overflow-hidden border border-gray-500 p-0 transition-transform duration-300 ease-in-out hover:scale-110"
+      onClick={onClick}
+    >
+      {src ? <img src={src} alt={alt} className="h-full w-full" /> : alt}
+    </button>
+  );
+};
 
 function ParentApp() {
   const [message, setMessage] = useState("");
   const [childMessage, setChildMessage] = useState("");
+  const [app, setApp] = useState<string>("/test-child");
   useEffect(() => {
     const handler = (ev: MessageEvent<{ type: string; message: string }>) => {
       if (typeof ev.data !== "object") return;
@@ -43,7 +69,31 @@ function ParentApp() {
         <Button label="Talk to child" onClick={() => clicked(message)} />
       </div>
       Child says: {childMessage}
-      <iframe id="viewer" src="/test-child" className="h-full w-full" />
+      <div className="flex flex-nowrap gap-2">
+        <AppIcon alt="Test App" onClick={() => setApp("/test-child")} />
+        <AppIcon
+          src={EnlightIcon}
+          alt="Enlight Icon"
+          onClick={() => setApp("/enlight")}
+        />
+        <AppIcon
+          src={MinesweeperIcon}
+          alt="Minesweeper Icon"
+          onClick={() => setApp("/minesweeper")}
+        />
+        <AppIcon
+          src={PathfinderVisualizerIcon}
+          alt="Pathfinder Visualizer Icon"
+          onClick={() => setApp("/pathfinder-visualizer")}
+        />
+        <AppIcon
+          src={AsciiVideoIcon}
+          alt="Matrix-Cam Icon"
+          onClick={() => setApp("/ascii-video")}
+        />
+        <AppIcon alt="dread ui" onClick={() => setApp("/dread-ui")} />
+      </div>
+      <iframe id="viewer" src={app} className="h-full w-full" />
     </div>
   );
 }
