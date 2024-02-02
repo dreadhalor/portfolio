6ae0972fc358e0d478a02d66261e7ca8a013cf93
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-01 18:35:29 -0800

    Finally got component library styles to sync across consuming apps Cleaning up config files

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Button, Button2 } from "test-ui";
 function App() {
   return (
     <div className="flex h-full w-full items-center justify-center border-8 border-black">
-      HI
-      <Button label="hey" />
-      <Button2>hey</Button2>
-      <button className="bg-red-500">huhsdf</button>
+      {/* HI */}
+      <Button label="Button" />
+      <Button2>Button2</Button2>
+      <button className="bg-red-500">button</button>
     </div>
   );
 }
